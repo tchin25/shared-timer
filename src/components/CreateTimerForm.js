95564522c412563569b178c7f85544963ec63142
Thomas Chin <tchin25@gmail.com>
2020-07-10 16:05:42 -0400

    streamlined css; started tinkering with inputs

--- src/components/CreateTimerForm.js
+++ src/components/CreateTimerForm.js
@@ -2,30 +2,47 @@ import React, { useContext } from "react";
 import { TimeContext } from "./../TimeContext";
 import moment from "moment";
 
+const labelCss =
+  "block uppercase tracking-wide text-gray-700 text-xs font-bold mt-2";
+
+const inputCss =
+  "appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500";
+
 const CreateTimerForm = (props) => {
   const timeContext = useContext(TimeContext);
 
   return (
     <form>
       <div>
-        <label for="party">End Date</label>
+        <label className={`${labelCss}`} for="end-date">
+          Set End Date
+        </label>
         <input
+          className={`${inputCss}`}
           id="end-date"
           type="datetime-local"
           name="end-date"
           min={moment(timeContext.currentTime).toISOString()}
-          required
+        ></input>
+      </div>
+      <div>
+        <label className={`${labelCss}`} for="countdown">
+          Timer Length
+        </label>
+        <input
+          className={`${inputCss}`}
+          id="countdown"
+          type="time"
+          name="countdown"
+          step="1"
         ></input>
       </div>
       <div className="">
-        <label
-          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="description"
-        >
+        <label className={`${labelCss}`} for="description">
           Description
         </label>
         <input
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          className={`${inputCss}`}
           id="description"
           type="text"
           placeholder="Description"
