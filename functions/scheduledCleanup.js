fad997650b4fdc74c89592618b1493050483dbe1
Thomas Chin <tchin25@gmail.com>
2020-07-12 17:05:37 -0400

    wrote example scheduled cloud function for cleanups

---
+++ functions/scheduledCleanup.js
@@ -0,0 +1,20 @@
+const functions = require("firebase-functions");
+const admin = require("./admin").initialize();
+
+// An example of a scheduled cloud function to remove old timers
+// Currently untested and not running
+exports.scheduledCleanup = functions.pubsub
+  .schedule("every 24 hours")
+  .onRun(async (context) => {
+    const moment = require("moment");
+    let currentTime = moment().valueOf();
+    const snapshot = await admin
+      .firestore()
+      .collection("timers")
+      .where("dueTime", "<", currentTime)
+      .get();
+
+    snapshot.forEach((doc) => {
+      doc.delete();
+    });
+  });
